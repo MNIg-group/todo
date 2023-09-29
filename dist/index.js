20b9553118b1bab13af3cd57964ef18db85482a9
Mugabe Ignace <ignacemugabe@gmail.com>
2023-09-29 19:28:42 +0200

    feat: create the backend of the project

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,6 @@
 /******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-console.log("Hello from Webpack");
+/******/ 	"use strict";
+/******/ 	
+/******/ 	
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSxrQyIsInNvdXJjZXMiOlsid2VicGFjazovL3RvZG8vLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJIZWxsbyBmcm9tIFdlYnBhY2tcIik7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
